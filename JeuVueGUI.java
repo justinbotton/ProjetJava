c155e797564cc50f622314ea1384086727b56f7a
DESKTOP-1RG61ND\Philemon <philemon.philj@gmail.com>
2017-12-15 19:34:26 +0100

    Première version GUI (corriger)

--- JeuVueGUI.java
+++ JeuVueGUI.java
@@ -1,60 +1,186 @@
 package info;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.image.ImageObserver;
-import java.io.File;
-import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import java.util.Observable;
+import java.util.Observer;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-/**
- * 
- * @author Philemon, Justin & Louis
- *
- */
+public class JeuVueGUI extends JeuVue implements ActionListener, Observer {
 
-public class JeuVueGUI {
-	public static class Fenetre extends JFrame{
-		public Fenetre() {
-			private JButton bouton = new JButton("Jouer");
-			
-			this.setTitle("Beat the dungeon");
-			this.setSize(1600, 800);
-			this.setLocationRelativeTo(null);
-			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			this.setContentPane(new Panneau());
-			
-			this.setVisible(true);
-		}
+	private JFrame jeuJFrame;
+	private JTextField numeroLivre = new JTextField(3);
+	JButton jouerB = new JButton("Jouer");
+	JButton chargerB = new JButton("Charger");
+	JButton quitterB = new JButton("Quitter");
+	JButton elfeB = new JButton("Elfe");
+	JButton nainB = new JButton("Nain");
+	JButton orqueB = new JButton("Orque");
+	JButton humainB = new JButton("Humain");
+	JFrame plateauCommande = new JFrame();
+	JLabel label = new JLabel("");
+	JLabel perso = new JLabel("");
+	JLabel joueur = new JLabel("");
+	Font font1 = new Font("Algerian", Font.BOLD, 100);
+	Font font2 = new Font("Algerian", Font.BOLD, 50);
+	int tourJoueur = 1;
+
+	public JeuVueGUI(Jeu model, JeuController jControl) {
+		
+		super(model, jControl);
+		//update(null, null);
+		
+		//Construction de la fenêtre
+		jeuJFrame = new JFrame(" ");
+		jeuJFrame.setTitle("Beat the Dungeon");
+		jeuJFrame.setSize(1600, 800);
+		jeuJFrame.setLocationRelativeTo(null);
+		jeuJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	    label.setPreferredSize(new Dimension(1600, 100));
+	    label.setText("Beat the Dungeon");
+	    label.setHorizontalAlignment(JLabel.CENTER);
+	    label.setVerticalAlignment(JLabel.TOP);
+	    label.setFont(font1);
+	    
+	    perso.setPreferredSize(new Dimension(1600, 100));
+	    perso.setHorizontalAlignment(JLabel.CENTER);
+	    perso.setVerticalAlignment(JLabel.TOP);
+	    perso.setFont(font2);
+	    
+	    jeuJFrame.add(label);
+
+	    JPanel panelbuttons = new JPanel();
+		panelbuttons.add(jouerB);
+		panelbuttons.add(chargerB);
+		panelbuttons.add(quitterB);
+		
+		jeuJFrame.add(panelbuttons, BorderLayout.SOUTH);
+	    
+		jeuJFrame.setVisible(true);
+		
+		jouerB.addActionListener(this);
+		chargerB.addActionListener(this);
+		quitterB.addActionListener(this);
+		elfeB.addActionListener(this);
+		nainB.addActionListener(this);
+		orqueB.addActionListener(this);
+		humainB.addActionListener(this);
+		//jeuJFrame.setContentPane(new Panneau());
+		/*textContent.setLayout(new BoxLayout(textContent, BoxLayout.Y_AXIS));
+		
+		
+		
+		jeuJFrame.add(textContent, BorderLayout.NORTH);
+		
+		JPanel fieldZone = new JPanel();
+		fieldZone.setLayout(new BoxLayout(fieldZone, BoxLayout.X_AXIS));
+		jeuJFrame.add(fieldZone, BorderLayout.CENTER);
+		
+		//Définition des actions sur les éléments de la GUI
+		emprunteJButton.addActionListener(this);
+		rendreJButton.addActionListener(this);
+		jeuJFrame.pack();*/
+		
+
+		
+		
 	}
-	public static class Panneau extends JPanel{
-		private static final ImageObserver Observer = null;
 
-		public void paintComponent(Graphics g){
-			g.fillRect(0, 0, 400, this.getHeight());
-			
-			Font font = new Font("Courier", Font.BOLD, 20);
-		    g.setFont(font);
-		    g.setColor(Color.WHITE);          
-		    g.drawString("Bienvenue dans Beat The Dungeon", 10, 20);    
-		    
-		    try {
-		        Image img = ImageIO.read(new File("C:/Users/Philemon/Pictures/Donjon/map3.png"));
-		        g.drawImage(img, 400, 0, this.getWidth() - 400, this.getHeight(), this);
-		    } 
-		    catch (IOException e) {
-		    	e.printStackTrace();
-		    }
-		  }             
+	public void affiche(String msg){
+		
+	}
+	public void updateTable(){
+		
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		updateTable();
+		//textContent.remove(1);
+		jeuJFrame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		 Object  source = e.getSource();
+	     if  (source == jouerB){
+	    	 onClickJouer();
+	     }
+	     else if (source == chargerB) {
+	    	 
+	     }    
+	     else if (source == quitterB) {
+	    	 onClickQuitter();
+	     }
+	     else if (source == elfeB) {
+	    	 onClickPersonnage(1);
+	     }
+	     else if (source == nainB) {
+	    	 onClickPersonnage(2);
+	     }
+	     else if (source == orqueB) {
+	    	 onClickPersonnage(3);
+	     }
+	     else if (source == humainB) {
+	    	 onClickPersonnage(4);
+	     }
+	     
 	}
 	
-	public static void main(String[] args){
-		Fenetre fen = new Fenetre();
-	}       
-
+	public void onClickJouer() {
+		jouerB.setVisible(false);
+		quitterB.setVisible(false);
+		chargerB.setVisible(false);
+		label.setVisible(false);
+		afficheChoixPerso(1);
+		
+	}
+	public void onClickCharger() {
+		
+	}
+	public void onClickQuitter() {
+		System.exit(0);
+	}
+	public void onClickPersonnage(int i) {
+		jControl.choixPersonnage(i);
+		elfeB.setVisible(false);
+		humainB.setVisible(false);
+		nainB.setVisible(false);
+		orqueB.setVisible(false);
+		perso.setText("Vous avez choisis : " + jControl.jeu.getJoueur().get(0).getClasse());
+		if(tourJoueur != 2) {
+			afficheChoixPerso(2);
+			tourJoueur++;
+		}
+		else {
+			perso.setText("La partie va commencer");
+		}
+	}
+	public void afficheChoixPerso(int i) {
+		
+		perso.setText("- Joueur " + i +" - choisissez votre personnage :");
+	    perso.setVisible(true);
+	    jeuJFrame.add(perso);
+	    
+		JPanel panelbuttons = new JPanel();
+		panelbuttons.add(elfeB);
+		panelbuttons.add(nainB);
+		panelbuttons.add(orqueB);
+		panelbuttons.add(humainB);
+		elfeB.setVisible(true);
+		humainB.setVisible(true);
+		nainB.setVisible(true);
+		orqueB.setVisible(true);
+		jeuJFrame.add(panelbuttons, BorderLayout.SOUTH);
+	}
+	public void affichePlateau() {
+		ImageIcon icone = new ImageIcon("C:/Users/Philemon/Pictures/Donjon/map3.png");
+		JLabel image = new JLabel(icone);
+	    jeuJFrame.add(image);
+	    jeuJFrame.setVisible(true);
+		
+				
+	}
 }
