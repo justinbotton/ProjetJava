3c2be27ffa9edb33390628ea04845c74e133fc34
lougerard <louis.gerard@student.uclouvain.be>
2017-12-15 22:03:10 +0100

    changment GUI ajout debut structure de la partie

--- JeuVueGUI.java
+++ JeuVueGUI.java
@@ -2,6 +2,7 @@ package info;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Observable;
 import java.util.Observer;
@@ -19,7 +20,15 @@ public class JeuVueGUI extends JeuVue implements ActionListener, Observer {
 	JButton nainB = new JButton("Nain");
 	JButton orqueB = new JButton("Orque");
 	JButton humainB = new JButton("Humain");
-	JFrame plateauCommande = new JFrame();
+	JButton joueur1B = new JButton("J1");
+	JButton joueur2B = new JButton("J2");
+	JButton ennemi1B = new JButton("E1");
+	JButton ennemi2B = new JButton("E2");
+	JButton ennemi3B = new JButton("E3");
+	JButton ennemi4B = new JButton("E4");
+	JButton ennemi5B = new JButton("E5");
+	ArrayList<JButton> ennemiList = new ArrayList<JButton>();
+	JButton bossB = new JButton("BOSS");
 	JLabel label = new JLabel("");
 	JLabel perso = new JLabel("");
 	JLabel joueur = new JLabel("");
@@ -31,11 +40,18 @@ public class JeuVueGUI extends JeuVue implements ActionListener, Observer {
 		
 		super(model, jControl);
 		//update(null, null);
-		
+		ennemiList.add(ennemi1B);
+		ennemiList.add(ennemi2B);
+		ennemiList.add(ennemi3B);
+		ennemiList.add(ennemi4B);
+		ennemiList.add(ennemi5B);
 		//Construction de la fenÃªtre
 		jeuJFrame = new JFrame(" ");
-		jeuJFrame.setTitle("Beat the Dungeon");
 		jeuJFrame.setSize(1600, 800);
+		jeuJFrame.setPreferredSize(new Dimension(1600,800));
+		jeuJFrame.setTitle("Beat the Dungeon");
+		
+		//jeuJFrame.setResizable(false);
 		jeuJFrame.setLocationRelativeTo(null);
 		jeuJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -126,7 +142,21 @@ public class JeuVueGUI extends JeuVue implements ActionListener, Observer {
 	     else if (source == humainB) {
 	    	 onClickPersonnage(4);
 	     }
-	     
+	     /*else if (source == ennemi1B) {
+	    	 onClickEnnemi(ennemi1B);
+	     }
+	     else if (source == ennemi2B) {
+	    	 onClickEnnemi(ennemi2B);
+	     }
+	     else if (source == ennemi3B) {
+	    	 onClickEnnemi(ennemi3B);
+	     }
+	     else if (source == ennemi4B) {
+	    	 onClickEnnemi(ennemi4B);
+	     }*/
+	     else if (ennemiList.contains(source)) {
+	    	 onClickEnnemi(source);
+	     }
 	}
 	
 	public void onClickJouer() {
@@ -149,13 +179,16 @@ public class JeuVueGUI extends JeuVue implements ActionListener, Observer {
 		humainB.setVisible(false);
 		nainB.setVisible(false);
 		orqueB.setVisible(false);
-		perso.setText("Vous avez choisis : " + jControl.jeu.getJoueur().get(0).getClasse());
+		//debug code
+		//perso.setText("Vous avez choisis : " + jControl.jeu.getJoueur().get(0).getClasse());
 		if(tourJoueur != 2) {
 			afficheChoixPerso(2);
 			tourJoueur++;
 		}
 		else {
-			perso.setText("La partie va commencer");
+			perso.setText("La partie va commencer...");
+			jeuBTD();
+			//affichePlateau();
 		}
 	}
 	public void afficheChoixPerso(int i) {
@@ -175,12 +208,68 @@ public class JeuVueGUI extends JeuVue implements ActionListener, Observer {
 		orqueB.setVisible(true);
 		jeuJFrame.add(panelbuttons, BorderLayout.SOUTH);
 	}
-	public void affichePlateau() {
-		ImageIcon icone = new ImageIcon("C:/Users/Philemon/Pictures/Donjon/map3.png");
+	//affiche le plateau a chaque nouveau donjon
+	public void affichePlateau(int num) {
+		perso.setVisible(false);
+		ImageIcon icone = new ImageIcon("C:/Users/louis/eclipse-workspace/TP2/src/info/map" + num +".png");
 		JLabel image = new JLabel(icone);
+		image.setSize(new Dimension(1400, 700));
 	    jeuJFrame.add(image);
 	    jeuJFrame.setVisible(true);
+	    joueur1B.setBounds(10, 10, 10, 10); // (x,y,width, height)
+	    joueur2B.setBounds(30, 10, 10, 10); 
+	    joueur1B.setVisible(true);
+	    joueur2B.setVisible(true);
+	    JPanel panelJoueur = new JPanel();
+	    panelJoueur.setAlignmentX(30);
+	    panelJoueur.setAlignmentY(30);
+	    panelJoueur.add(joueur1B);
+	    panelJoueur.add(joueur2B);
+	    jeuJFrame.add(panelJoueur);
+	}
+	public void chargement() {
+		JLabel charge = new JLabel("");
+		charge.setPreferredSize(new Dimension(1600, 100));
+		charge.setHorizontalAlignment(JLabel.CENTER);
+		charge.setVerticalAlignment(JLabel.TOP);
+		charge.setFont(font2);
+		charge.setText("Initialisation du jeu ..............");
+		charge.setVisible(true);
+		jeuJFrame.add(charge);
+	}
+	public void onClickEnnemi(Object source) {
 		
-				
+	}
+	
+	// fait tourner la partie une fois les perso choisi
+	public void jeuBTD() {
+		affichePlateau(jControl.jeu.getDonjonNum());
+		jControl.creationDonjons();
+		Donjon d = jControl.jeu.getDonj();
+		while (jControl.jeu.getEnVie() > 0 && jControl.jeu.getDonjonNum() <= 5 ) {
+			if (jControl.jeu.getDonjonNum() == 1) {
+				ennemi1B.setText(d.getVague1()[0].getClasse());
+				ennemi2B.setText(d.getVague1()[1].getClasse());
+				ennemi3B.setText(d.getVague1()[2].getClasse());
+				ennemi1B.setVisible(true);
+				ennemi2B.setVisible(true);
+				ennemi3B.setVisible(true);
+				JPanel panelEnnemi = new JPanel();
+				panelEnnemi.add(ennemi1B);
+				panelEnnemi.add(ennemi2B);
+				panelEnnemi.add(ennemi3B);
+				jeuJFrame.add(panelEnnemi);
+			}
+			jControl.jeu.setEnVie(0);
+			jeuBTD();
+		}
+		if (jControl.jeu.getEnVie() == 0){
+			perso.setText("Vous avez perdu !");
+			perso.setVisible(true);
+		}
+		else {
+			perso.setText("Vous avez gagné !");
+			perso.setVisible(true);
+		}
 	}
 }
